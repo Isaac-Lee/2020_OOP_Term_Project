688c340e5307ac997211030114347e578cbf54a1
isaac_lee <yy0221ss@gmail.com>
2020-11-07 22:22:59 +0900

    card 해더 클래스 파일 작성

---
+++ card.cpp
@@ -0,0 +1,32 @@
+#include "card.h"
+#include <string>
+
+Card::Card(std::string name, bool gwang, bool yeolkkeut, bool godori, int pi, std::string tti) {
+  setCard(name, gwang, yeolkkeut, godori, pi, _tti);
+}
+
+void Card::setCard(std::string name, bool gwang, bool yeolkkeut, bool godori, int pi, std::string tti) {
+  _name = name;
+  _gwang = gwang;
+  _yeolkkeut = yeolkkeut;
+  _godori = godori;
+  _pi = pi;
+  _tti = tti;
+}
+
+CardInfo Card::getCardInfo() {
+  CardInfo card_info;
+  
+  card_info.name = _name;
+  card_info.gwang = _gwang;
+  card_info.yeolkkeut = _yeolkkeut;
+  card_info.godori = _godori;
+  card_info.pi = _pi;
+  card_info.tti = _tti;
+
+  return card_info;
+}
+
+std::string Card::ToString() {
+  return _name + " " + std::to_string(_id);
+}
