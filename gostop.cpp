aa9a407199b94e13782c21ddebdeff255c66ca1d
isaac_lee <yy0221ss@gmail.com>
2020-11-07 22:42:55 +0900

    make gostop file

---
+++ gostop.cpp
@@ -0,0 +1,12 @@
+#include "card.h"
+
+int main(int argc, char const *argv[])
+{
+  Card cards[48];
+  // TODO
+  // card.txt 파일에 순서 대로 name, gwang, yeolkkeut, godori, pi, _tti를 개행문자로 나누어서 작성
+  // 예시로 하나 작성해둠.
+  // 이 gostop.cpp 파일에 해당 card.txt 파일을 읽어서 카드 정보를 카드 객체로 만들어서 리스트에 넣는 코드 작성
+  // for문 써써 파일 입력 받으시면 됩니다. 카드 id는 그냥 for문 돌리면서 사용하는 i로 작성
+  return 0;
+}
